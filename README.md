c613784b598608078eb1673919ef6192cdf0424e
Roch D'Amour <roch.damour@gmail.com>
2018-10-27 04:34:55 -0400

    Added "Selenium-Spammer"

---
+++ README.md
@@ -0,0 +1,7 @@
+# Random scripts
+
+## Python:
+
+#### selenium-spammer 
+
+Load a web page and try to login `n` times directly in firefox. Require selenium and geckodriver
---
+++ selenium-spammer.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+message = "You are a nice person"
+
+count = 100
+
+driver = webdriver.Firefox()
+
+while count != 0:
+    driver.get("http://website-to-spam.xyz")
+
+    u = driver.find_element_by_id("username")
+    p = driver.find_element_by_id("password")
+
+    u.send_keys(message)
+    p.send_keys(message)
+
+    p.send_keys(Keys.RETURN)
+
+    coun = count - 1
+
+driver.close()
+
+
